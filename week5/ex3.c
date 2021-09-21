d0268bb75489e2e4a6c098bb195ab8933dd2e43a
Konstantin Smirnov <mrskvayzer@gmail.com>
2021-09-21 23:54:13 +0300

    week5

--- week5/ex3.c
+++ week5/ex3.c
@@ -2,12 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 #define N 10
 
 int buffer[N];
 int position=0;
-int bConsumerSleeping, bProducerSleeping;
+int bConsumerSleeping=0, bProducerSleeping=1;
 
+void printBuffer(){
+    printf("\nBuffer now: ");
+    for(int i=0;i<N;i++){
+        printf("%d ", buffer[i]);
+        fflush(stdout);
+    }
+}
 void* produce(void* args){
     while(1){
         if(bProducerSleeping){
@@ -18,8 +31,9 @@ void* produce(void* args){
             bConsumerSleeping = 0;
             continue;
         }
-        buffer[position++]=rand();
-
+        buffer[++position]=rand();
+        printBuffer();
+//         sleep(1);
     }
 }
 
@@ -34,6 +48,9 @@ void* consume(void* args){
             continue;
         }
         buffer[position--] = 0;
+        printBuffer();
+
+//         sleep(1);
     }
 }
 
@@ -42,6 +59,7 @@ int main(){
 
     pthread_create(&consumer, NULL, consume, NULL);
     pthread_create(&producer, NULL, consume, NULL);
+    printBuffer();
 
     pthread_join(producer, NULL);
     pthread_join(consumer, NULL);
