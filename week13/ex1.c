0c1cc8c3d1be262473b615235b05a6be6ec20601
Konstantin Smirnov <mrskvayzer@gmail.com>
2021-11-16 20:35:28 +0300

    week13

--- week13/ex1.c
+++ week13/ex1.c
@@ -8,16 +8,25 @@
 
 
 
-int* readLine(char* line, int m){
+int* readLine(char* line, int* m){
+    char* lineCopy = strdup(line);
     char* split = strtok(line, " ");
     int j=0;
-    int* lineData = malloc(m * sizeof(int));
+    if((*m) == 0){
+        while(split != NULL){
+            (*m)++;
+            split = strtok(NULL, " ");
+        }
 
-    while(split != NULL && j<m){
+    }
+    int* lineData = malloc((*m) * sizeof(int));
+    split = strtok(lineCopy, " ");
+    while(split != NULL && j<(*m)){
         lineData[j] = atoi(split);
         split = strtok(NULL, " ");
         j++;
     }
+    free(lineCopy);
     return lineData;
 }
 int main(){
@@ -31,26 +40,22 @@ int main(){
 
         exit(EXIT_FAILURE);
     }
-    int n, m;
+    int n=0, m=0;
 
-    printf("Type the number of processes and the number of types of resources: \n");
-    scanf("%d", &n);
-    scanf("%d", &m);
+//     printf("Type the number of processes and the number of types of resources: \n");
+//     scanf("%d", &n);
+//     scanf("%d", &m);
 
     printf("\n");
 
-    int* exists = malloc(m * sizeof(int));
-    int* available = malloc(m * sizeof(int));
+    int* exists;
+    int* available;
 
-    int** request = malloc(n * sizeof(int*));
-    int** current = malloc(n * sizeof(int*));
-    int* terminated = malloc(n * sizeof(int));
-    for(int i=0;i<n;i++) terminated[i]=0;
+
 
     int counter = 0;
     int process_counter = 0;
     int flag = 1;
-
 
 
 
@@ -59,17 +64,50 @@ int main(){
             flag = 1;
             switch(counter){
                 case 0:
-                    exists = readLine(line, m);
-                    break;
                 case 1:
-                    available = readLine(line, m);
                     break;
                 case 2:
-                    current[process_counter] = readLine(line, m);
+                    break;
+                case 3:
+                    n++;
+                    break;
+            }
+
+        }else{
+            if(flag) {
+                flag = 0;
+                counter++;
+            }
+
+        }
+
+    }
+    flag = 1;
+    counter = 0;
+    fclose(fp);
+    fp = fopen("input.txt", "r");
+
+    int** request = malloc(n * sizeof(int*));
+    int** current = malloc(n * sizeof(int*));
+    int* terminated = malloc(n * sizeof(int));
+    for(int i=0;i<n;i++) terminated[i]=0;
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if(strlen(line)>1){
+            flag = 1;
+            switch(counter){
+                case 0:
+                    exists = readLine(line, &m);
+                    break;
+                case 1:
+                    available = readLine(line, &m);
+                    break;
+                case 2:
+                    current[process_counter] = readLine(line, &m);
                     process_counter++;
                     break;
                 case 3:
-                    request[process_counter] = readLine(line, m);
+                    request[process_counter] = readLine(line, &m);
                     process_counter++;
                     break;
             }
@@ -84,30 +122,32 @@ int main(){
 
     }
 
-//     for(int i=0;i<m;i++){
-//         printf("%d ", exists[i]);
-//     }
-//     printf("\n");
-//     for(int i=0;i<m;i++){
-//         printf("%d ", available[i]);
-//     }
-//     printf("\n");
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<m;j++){
-//             printf("%d ", current[i][j]);
+//    for(int i=0;i<m;i++){
+//        printf("%d ", exists[i]);
+//    }
+//    printf("\n\n");
+//    for(int i=0;i<m;i++){
+//        printf("%d ", available[i]);
+//    }
+//    printf("\n\n");
+//    for(int i=0;i<n;i++){
+//        for(int j=0;j<m;j++){
+//            printf("%d ", current[i][j]);
 //
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
-//     for(int i=0;i<n;i++){
-//         for(int j=0;j<m;j++){
-//             printf("%d ", request[i][j]);
+//        }
+//        printf("\n");
+//    }
+//    printf("\n\n");
+//    for(int i=0;i<n;i++){
+//        for(int j=0;j<m;j++){
+//            printf("%d ", request[i][j]);
 //
-//         }
-//         printf("\n");
-//     }
-//     printf("\n");
+//        }
+//        printf("\n");
+//    }
+//    printf("\n\n");
+
+    printf("n: %d, m: %d \n", n, m);
     int deadlock = 0;
     for(int k=0;k<n;k++){
         int isAnyTerminated = 0;
